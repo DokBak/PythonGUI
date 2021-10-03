a3fc961de6524e601ff6aeb0334e3072e6632be2
JeongMinGi <photoismylife0216@gmail.com>
2021-10-03 22:16:52 +0900

    2021년 10월 3일 파이썬 GUI프로그램_버그수정

--- gui_project/5_apply_options.py
+++ gui_project/5_apply_options.py
@@ -27,6 +27,7 @@ def del_file():
 def browse_dest_path():
     folder_selected = filedialog.askdirectory()
     if folder_selected == "": # 사용자가 취소를 누를 때
+        print("폴더 선택 취소")
         return
     print(folder_selected)
     txt_dest_path.delete(0,END)
@@ -34,88 +35,90 @@ def browse_dest_path():
 
 # 이미지 통합
 def merge_image():
-    print("가로 넓이 : ", cmb_width.get())
-    print("간격 : ",cmb_space.get())
-    print("포멧 : ",cmb_format.get())
+    try:
+        print("가로 넓이 : ", cmb_width.get())
+        print("간격 : ",cmb_space.get())
+        print("포멧 : ",cmb_format.get())
 
-    # 가로 넓이
-    img_width = cmb_width.get()
-    if img_width == "원본유지":
-        img_width = -1 # -1 일때는 원본 기준으로
-    else:
-        img_width = int(img_width)
+        # 가로 넓이
+        img_width = cmb_width.get()
+        if img_width == "원본유지":
+            img_width = -1 # -1 일때는 원본 기준으로
+        else:
+            img_width = int(img_width)
 
-    # 간격
-    img_space = cmb_space.get()
-    if img_space == "좁게":
-        img_space = 30
-    elif img_space == "보통":
-        img_space = 60
-    elif img_space == "넓게":
-        img_space = 90
-    else: #업음
-        img_space = 0
+        # 간격
+        img_space = cmb_space.get()
+        if img_space == "좁게":
+            img_space = 30
+        elif img_space == "보통":
+            img_space = 60
+        elif img_space == "넓게":
+            img_space = 90
+        else: #업음
+            img_space = 0
 
-    # 포멧
-    img_format = cmb_format.get().lower()  #PNG, JPG, BMP 값을 받아와서 소문자로 변경
+        # 포멧
+        img_format = cmb_format.get().lower()  #PNG, JPG, BMP 값을 받아와서 소문자로 변경
 
-    print(list_file.get(0,END)) # 모든 파일 목록을 가지고 오기 
-    images = [Image.open(x) for x in list_file.get(0,END)]
-    # size -> size[0], width, size[1] : height
+        print(list_file.get(0,END)) # 모든 파일 목록을 가지고 오기 
+        images = [Image.open(x) for x in list_file.get(0,END)]
+        # size -> size[0], width, size[1] : height
 
-    # 이미지 사이즈 리스트에 넣어서 하나씩 처리
-    image_sizes=[] # (width1,height1), (width2, height2),...
-    if img_width > -1:
-        # width 값 변경
-        image_sizes = [(int(img_width), int(img_width * x.size[1] / x.size[0])) for x in images]
-    else:
-        # 원본 사이즈 사용
-        image_sizes = [(x.size[0],x.size[1]) for x in images]
-
-    # 이미지 변경시 계산식
-    # 100 * 60 이미지가 있음. -> width를 80으로 줄이면 height는?
-    # (원본 width) : (원본 height) = (변경 width) : (변경 height)
-
-    # 이미지들의 각각의 가로 세로 높이를 가져온다.
-    # zip 설명 이전 코드 # 
-        # widths = [x.size[0] for x in images] 
-        # heights = [x.size[1] for x in images]
-        # print("width : ", widths)
-        # print("heights : ", heights)
-    # zip 설명 이전 코드 #
-    widths, heights = zip(*(image_sizes))
-
-    # 가로길이는 이미지들중에서 최대값, 세로길이는 모든 이미지들의 합
-    max_width, total_height = max(widths), sum(heights)
-    print("max_width : ", max_width)
-    print("total_heigth : ", total_height)
-
-    # 스케치북 준비
-    if img_space >0: #이미지 간격 옵션 적용
-        total_height +=(img_space * (len(images) - 1))
-
-    result_img = Image.new("RGB", (max_width,total_height), (255,255,255)) # 새로운 스케치북 만들기("RGB", 크기, 배경색)
-    y_offset = 0 # y 위치 정보 
-    # for img in images:
-    #     result_img.paste(img,(0,y_offset))
-    #     y_offset += img.size[1] # height 값 만큼 더해줌
-
-    for idx, img in enumerate(images):
-        # width가 원본유지가 아닐 때에는 이미지 크기 조절
+        # 이미지 사이즈 리스트에 넣어서 하나씩 처리
+        image_sizes=[] # (width1,height1), (width2, height2),...
         if img_width > -1:
-            img = img.resize(image_sizes[idx])
-        result_img.paste(img,(0,y_offset))
-        y_offset += img.size[1] + img_space # height값 + 사용자가 지정한 간격
+            # width 값 변경
+            image_sizes = [(int(img_width), int(img_width * x.size[1] / x.size[0])) for x in images]
+        else:
+            # 원본 사이즈 사용
+            image_sizes = [(x.size[0],x.size[1]) for x in images]
 
-        progress = (idx + 1)/len(images) * 100 # 실제 percent 정보를 계산 
-        p_var.set(progress)
-        progress_bar.update
-    # 포멧 옵션 처리
-    file_name = "DokBak." + img_format
-    dest_path = os.path.join(txt_dest_path.get(),file_name)
-    result_img.save(dest_path)
-    msgbox.showinfo("알림", "작업이 완료되었습니다.")
+        # 이미지 변경시 계산식
+        # 100 * 60 이미지가 있음. -> width를 80으로 줄이면 height는?
+        # (원본 width) : (원본 height) = (변경 width) : (변경 height)
 
+        # 이미지들의 각각의 가로 세로 높이를 가져온다.
+        # zip 설명 이전 코드 # 
+            # widths = [x.size[0] for x in images] 
+            # heights = [x.size[1] for x in images]
+            # print("width : ", widths)
+            # print("heights : ", heights)
+        # zip 설명 이전 코드 #
+        widths, heights = zip(*(image_sizes))
+
+        # 가로길이는 이미지들중에서 최대값, 세로길이는 모든 이미지들의 합
+        max_width, total_height = max(widths), sum(heights)
+        print("max_width : ", max_width)
+        print("total_heigth : ", total_height)
+
+        # 스케치북 준비
+        if img_space >0: #이미지 간격 옵션 적용
+            total_height +=(img_space * (len(images) - 1))
+
+        result_img = Image.new("RGB", (max_width,total_height), (255,255,255)) # 새로운 스케치북 만들기("RGB", 크기, 배경색)
+        y_offset = 0 # y 위치 정보 
+        # for img in images:
+        #     result_img.paste(img,(0,y_offset))
+        #     y_offset += img.size[1] # height 값 만큼 더해줌
+
+        for idx, img in enumerate(images):
+            # width가 원본유지가 아닐 때에는 이미지 크기 조절
+            if img_width > -1:
+                img = img.resize(image_sizes[idx])
+            result_img.paste(img,(0,y_offset))
+            y_offset += img.size[1] + img_space # height값 + 사용자가 지정한 간격
+
+            progress = (idx + 1)/len(images) * 100 # 실제 percent 정보를 계산 
+            p_var.set(progress)
+            progress_bar.update
+        # 포멧 옵션 처리
+        file_name = "DokBak." + img_format
+        dest_path = os.path.join(txt_dest_path.get(),file_name)
+        result_img.save(dest_path)
+        msgbox.showinfo("알림", "작업이 완료되었습니다.")
+    except Exception as err: # 예외 처리
+        msgbox.showerror("에러", err)
 # 시작
 def start():
     # 각 옵션들 값을 확인
